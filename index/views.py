a118e6ae8ba8c7d111da7bcc563f0e79a71aaace
Luissalamanca23 <user.email like : "l.salamanca18@icloud.com>
2024-06-07 09:37:55 -0400

    mejora funciones de vista

--- index/views.py
+++ index/views.py
@@ -1,24 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Gato
 
-# Create your views here.
-
-
-def index(retquest):
+def index(request):
     contexto = {'mensaje': 'Pagina de inicio'}
-    return render(retquest, 'index.html');
+    return render(request, 'index.html')
 
-def animales_gato(retquest):
+def animales_gato(request):
     contexto = {'mensaje': 'Pagina de gatos'}
-    return render(retquest, 'animales_gato.html');
+    return render(request, 'animales_gato.html')
 
-def animales_perro(retquest):
+def animales_perro(request):
     contexto = {'mensaje': 'Pagina de perros'}
-    return render(retquest, 'animales_perro.html');
+    return render(request, 'animales_perro.html')
 
-def perros(retquest):
+def perros(request):
     contexto = {'mensaje': 'Pagina de perros'}
-    return render(retquest, 'perros.html');
+    return render(request, 'perros.html')
 
-def gatos(retquest):
-    contexto = {'mensaje': 'Pagina de gatos'}
-    return render(retquest, 'gatos.html');
+def gato(request, slug):
+    gato = get_object_or_404(Gato, slug=slug)
+    contexto = {'gato': gato, 'mensaje': 'Pagina de gatos'}
+    return render(request, 'gatos.html', contexto)
+
+def login(request):
+    contexto = {'mensaje': 'Pagina de login'}
+    return render(request, 'login.html')
