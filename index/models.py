06ddc978648a69a4b7ef93964785ef72fb238af9
Luissalamanca23 <user.email like : "l.salamanca18@icloud.com>
2024-06-07 09:17:26 -0400

    bases de datos gatos

--- index/models.py
+++ index/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class gato(models.Model):
+    id_gato = models.AutoField(primary_key=True);
+    nombre = models.CharField(max_length=100);
+    edad = models.IntegerField();
+    raza = models.CharField(max_length=100);
+    esterilizado = models.BooleanField();
 
+
+    def __str__(self):
+ 
